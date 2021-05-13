87579d316d80a94680dee8b5179ffeeba6d907e9
Pardeep Sanghera <pradeepsanghera@gmail.com>
2021-05-13 14:39:35 -0700

    feat(single-card and single-drink) passed props to single card

--- src/components/SingleDrink.tsx
+++ src/components/SingleDrink.tsx
@@ -1,17 +1,55 @@
-import React, { useContext } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { GlobalContext } from '../context/GlobalContext';
+import '../App.css';
 
-
-interface CardProps {
-  
+interface SingleDrink{
+  drink: Drinks;
 }
 
-const SingleDrinkCard:any = () => {
-  return (
-    <div>
-    </div>
+const SingleDrinkCard : React.FC <SingleDrink> = ({drink}) =>{
+    console.log('hi',drink);
+    return(
+        <div className="card d-flex bg-dark my-4 col w-70 justify-content-between text-center text-white">
+            <div className="card-body">
+                <img className="w-50" src={drink.strDrinkThumb} alt={drink.strDrink} />  
+            </div>   
+            <h5>Ingredients:</h5>
+            <div>{drink.strIngredient1}
+            </div>                     
+            <div>{drink.strIngredient2}
+            </div>                     
+            <div>{drink.strIngredient3}
+            </div>                     
+            <div>{drink.strIngredient4}
+            </div>                     
+            <div>{drink.strIngredient5}
+            </div>                     
+            <div>{drink.strIngredient6}
+            </div>                     
+            <div>{drink.strIngredient7}
+            </div>                     
+            <div>{drink.strIngredient8}
+            </div>                     
+            <div>{drink.strIngredient9}
+            </div>                     
+            <div>{drink.strIngredient10}
+            </div>                     
+            <div>{drink.strIngredient11}
+            </div>                     
+            <div>{drink.strIngredient12}
+            </div>                     
+            <div>{drink.strIngredient13}
+            </div>                     
+            <div>{drink.strIngredient14}
+            </div>                     
+            <div>{drink.strIngredient15}
+            </div> 
+            <br />
 
-  );
-};
 
-export default SingleDrinkCard;
+            
+        </div>
+    )
+}
+
+export default SingleDrinkCard; 
